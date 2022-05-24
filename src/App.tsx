0ce5e96c882d6efdd6ff27d58bccfe381ae5b574
Sophia Sales <sophiabrenda10@gmail.com>
2022-05-24 15:24:39 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+import SignIn from './pages/SignIn';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <SignIn />
+    </SafeAreaView>
+  );
+};
+
+export default App;
