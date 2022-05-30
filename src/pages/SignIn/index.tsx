4603ff778729d3fb6785f50b65249f29ff147334
Sophia Sales <sophiabrenda10@gmail.com>
2022-05-30 14:13:01 -0300

    navegaçao entre telas pansando user e senha

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -6,21 +6,23 @@ import {styles} from './styles';
 import {useNavigation} from '@react-navigation/native';
 
 import {propsStack} from '../../routes';
+import {Requests} from '../Requests';
 
 export function SignIn() {
   const [usuario, setUsuario] = useState('');
   const [senha, setSenha] = useState('');
   const navigation = useNavigation<propsStack>();
 
-  // const segre = '1234';
-  // const user = 'adm';
-  // const nextPage = () => {
-  //   if (user === usuario && segre === senha) {
-  //     navigation.navigate('Requests', {
-  //       usuario: usuario,
-  //       senha: senha,
-  //     });
-  //   }
+  const segre = '1234';
+  const user = 'adm';
+  const nextPage = () => {
+    if (user === usuario && segre === senha) {
+      navigation.reset({
+        index: 0,
+        routes: [{name: 'Requests'}],
+      });
+    }
+  };
   return (
     <View style={styles.container}>
       <View style={styles.box}>
@@ -31,14 +33,7 @@ export function SignIn() {
           placeholder="Senha"
           onChangeText={text => setSenha(text)}
         />
-        <Button
-          onPress={() =>
-            navigation.navigate('Requests', {
-              usuario: usuario,
-              senha: senha,
-            })
-          }
-        />
+        <Button onPress={nextPage} />
       </View>
     </View>
   );
