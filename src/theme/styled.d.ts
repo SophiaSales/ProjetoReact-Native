87f4ea37bb4624dba4a7c6113cbec86ef157e9f6
Sophia Sales <sophiabrenda10@gmail.com>
2023-03-29 20:55:26 -0300

    separando por pastas

--- src/theme/styled.d.ts
+++
@@ -1,8 +0,0 @@
-import 'styled-components'
-import themes from './themes'
-
-declare module 'styled-components' {
-    type ThemeType = typeof themes
-
-    export interface DefaultTheme extends ThemeType { }
-}
