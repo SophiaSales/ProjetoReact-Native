eebec22c704fcb1a71119344a3a91f600e8eed1d
Wilman Martinez <wilman.martinez@onerf.com.br>
2023-04-06 19:57:19 -0300

    Arquivo de routes

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const server = express();
+const routes = require("./routes/routes");
 const PORT = 3000;
 const local = "[Server]";
 
 server.use(express.json());
+server.use(routes);
 
 try {
     server.listen(PORT, () => {
