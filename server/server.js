f2e1840511542849d865221ac199c75865c55242
Wilman Martinez <wilman.martinez@onerf.com.br>
2023-04-25 17:50:10 -0300

    Salvando alterações

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const dbConnection = require("./src/config/dbConnection");
 const server = express();
 const ordersRoutes = require("./src/routes/ordersRoutes");
 const usersRoutes = require("./src/routes/usersRoutes");
+const loginRoutes = require("./src/routes/loginRoutes");
 const database = require("./src/config/dbConnection");
 const PORT = 3000;
 const local = "[Server]";
@@ -11,6 +12,7 @@ const local = "[Server]";
 server.use(express.json());
 server.use(ordersRoutes);
 server.use(usersRoutes);
+server.use(loginRoutes);
 
 const serverConfig = async (uri) => {
     try {
