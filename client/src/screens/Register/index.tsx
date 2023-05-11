0ea9c612dbddbc62342aa2c0e5e7303d65e3e73d
Sophia Sales <sophiabrenda10@gmail.com>
2023-05-10 23:28:58 -0300

    integracao tela de cadrastro

--- client/src/screens/Register/index.tsx
+++ client/src/screens/Register/index.tsx
@@ -1,11 +1,29 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Container, Box, Input, Title, Botton, BottonTitle} from './styles';
+import {api} from '../../libs/api';
+import {Text} from 'react-native';
 
 export function Register() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [data, setData] = useState();
+  const [error, setError] = useState('');
+  const getUsers = async () => {
+    try {
+      const response = await api.post('/register', {
+        email: email,
+        password: password,
+      });
+      setData(response.data);
+    } catch (error) {
+      setError(
+        'Erro durante o cadastro do usuário. Por favor, tente novamente.',
+      );
+      console.error('Erro durante a chamada à API:', error);
+    }
+  };
 
   return (
     <Container>
@@ -21,7 +39,8 @@ export function Register() {
           placeholder="Confirme Senha"
           secureTextEntry
           onChangeText={setConfirmPassword}></Input>
-        <Botton>
+        {error !== '' && <Text style={{color: 'red'}}>{error}</Text>}
+        <Botton onPress={getUsers}>
           <BottonTitle>Cadastrar</BottonTitle>
         </Botton>
       </Box>
